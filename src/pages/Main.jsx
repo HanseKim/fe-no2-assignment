50f981e91b2102f172755df218f13d2516568c36
HanseKim <kimbird2002@naver.com>
2025-05-21 14:27:21 +0900

    image 다운

---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import React from 'react';
+import logo from '../images/pokemon-logo.png';
+
+const Main = () => {
+  return (
+    <div>
+      <img src={logo} alt="" />
+    </div>
+  );
+};
+
+export default Main;
